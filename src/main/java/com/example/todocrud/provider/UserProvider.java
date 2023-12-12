539f1cbb844d410162c00636dd525e1625f9cc40
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2023-12-12 13:42:43 +0100

    Adding ConnectionUsernameProvider

--- src/main/java/com/example/todocrud/provider/UserProvider.java
+++ src/main/java/com/example/todocrud/provider/UserProvider.java
@@ -2,13 +2,15 @@ package com.example.todocrud.provider;
 
 import org.springframework.stereotype.Component;
 import org.springframework.data.jdbc.support.ConnectionUsernameProvider;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.userdetails.UserDetails; 
 
 @Component
 class SecurityContextHolderUserProvider implements ConnectionUsernameProvider {
      @Override
      public String getUserName() {
-         Object principal = SecurityContextHolder.context.authentication.principal;
+         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
          
          if (principal instanceof UserDetails) {
              return ((UserDetails)principal).getUsername();
