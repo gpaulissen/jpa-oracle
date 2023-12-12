ac04ade3d0a55bcdc2ac920fa2318b64dabcd39e
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2023-12-12 14:57:19 +0100

    Added support for proxy connections.

--- src/main/java/com/example/todocrud/configuration/OracleConfiguration.java
+++ src/main/java/com/example/todocrud/configuration/OracleConfiguration.java
@@ -2,39 +2,69 @@ package com.example.todocrud.configuration;
 
 // import org.springframework.stereotype.Component;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-import oracle.jdbc.pool.OracleDataSource;
-import org.springframework.data.jdbc.support.ConnectionUsernameProvider;
-import javax.sql.DataSource;
 import java.sql.SQLException;
-import  org.springframework.beans.factory.annotation.Autowired;
+import javax.sql.DataSource;
+import javax.validation.constraints.NotNull;
+import oracle.jdbc.pool.OracleDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jdbc.support.ConnectionUsernameProvider;
 import org.springframework.data.jdbc.support.oracle.ProxyDataSource;
+import org.springframework.util.Assert;
 
+import com.example.todocrud.provider.SecurityContextHolderUserProvider;
+    
 @Configuration
-@ConfigurationProperties("spring")
-class OracleConfiguration {
+@ConfigurationProperties("oracle")
+public class OracleConfiguration {
     @Autowired
-    private ConnectionUsernameProvider contextProvider;
-    private OracleDataSource dataSourceInternal;
-    String username;
-    String password;
-    String url;
+    private ConnectionUsernameProvider contextProvider; // = new SecurityContextHolderUserProvider();
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    private String url;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    
     @Bean
-    public OracleDataSource oracleDataSource() throws SQLException {
-        dataSourceInternal.setUser(username);
-        dataSourceInternal.setPassword(password);
-        dataSourceInternal.setURL(url);
-        dataSourceInternal.setImplicitCachingEnabled(true);
-        // dataSourceInternal.setFastConnectionFailoverEnabled(true);
-        return dataSourceInternal;
+    OracleDataSource oracleDataSource() throws SQLException {
+        OracleDataSource dataSource = new OracleDataSource();
+
+        Assert.notNull(username, "Username must NOT be null");
+        Assert.notNull(password, "Password must NOT be null");
+        Assert.notNull(url, "URL must NOT be null");
+
+        dataSource.setUser(username);
+        dataSource.setPassword(password);
+        dataSource.setURL(url);
+        dataSource.setImplicitCachingEnabled(true);
+        // dataSource.setFastConnectionFailoverEnabled(true);
+
+        return dataSource;
     }
     
     @Bean
     @Primary
-    public DataSource dataSource() throws SQLException {
-        ProxyDataSource dataSource = new ProxyDataSource(dataSourceInternal, contextProvider);
-        return dataSource;
+    DataSource dataSource() throws SQLException {
+        Assert.notNull(contextProvider, "Context provider must NOT be null");
+        
+        return new ProxyDataSource(oracleDataSource(), contextProvider);
     }
 }
